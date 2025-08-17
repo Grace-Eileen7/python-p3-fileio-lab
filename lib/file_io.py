3787604c7c10bed5c3662528ce3516b2a2c4b566
Grace Eileen Bass <grace.eilee.bass@gmail.com>
2025-08-18 01:05:21 +0300

    Implement file IO functions (write, append, read)

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = f"{file_name}.txt"
+    with open(file_name, "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_name = f"{file_name}.txt"
+    with open(file_name, "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name = f"{file_name}.txt"
+    with open(file_name, "r") as f:
+        return f.read()
